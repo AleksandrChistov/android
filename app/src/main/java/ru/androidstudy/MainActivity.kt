1b734aaccb2a0875c006fd33b498230ccbcb9b7c
Александр Чистов <aleksandrchistov.ru@yandex.ru>
2024-09-09 18:41:00 +0700

    Create Calculator App

--- app/src/main/java/ru/androidstudy/MainActivity.kt
+++ app/src/main/java/ru/androidstudy/MainActivity.kt
@@ -1,23 +1,62 @@
 package ru.androidstudy
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.ImageButton
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
-    private val helloText by lazy { findViewById<TextView>(R.id.helloText) }
-    private val startButton by lazy { findViewById<ImageButton>(R.id.startButton) }
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private val buttonAdd by lazy { findViewById<Button>(R.id.buttonAdd) }
+    private val buttonSubtract by lazy { findViewById<Button>(R.id.buttonSubtract) }
+    private val buttonDivide by lazy { findViewById<Button>(R.id.buttonDivide) }
+    private val buttonMultiply by lazy { findViewById<Button>(R.id.buttonMultiply) }
+    private val buttonClean by lazy { findViewById<Button>(R.id.buttonClean) }
+
+    private val result by lazy { findViewById<TextView>(R.id.result) }
+    private val operation by lazy { findViewById<TextView>(R.id.operation) }
+    private val number1 by lazy { findViewById<EditText>(R.id.number1) }
+    private val number2 by lazy { findViewById<EditText>(R.id.number2) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        buttonAdd.setOnClickListener(this)
+        buttonSubtract.setOnClickListener(this)
+        buttonDivide.setOnClickListener(this)
+        buttonMultiply.setOnClickListener(this)
+        buttonClean.setOnClickListener(this)
     }
 
-    fun onStartButtonClick(view: View) {
-        startButton.setImageResource(R.drawable.ic_launcher_foreground)
-        helloText.visibility = View.VISIBLE
-        startButton.isClickable = false
+    override fun onClick(v: View?) {
+        var res = 0f
+        val num1 = number1.text.toString().toFloatOrNull() ?: 0f
+        val num2 = number2.text.toString().toFloatOrNull() ?: 0f
+        when (v?.id) {
+            R.id.buttonAdd -> {
+                operation.text = "+"
+                res = num1 + num2
+            }
+            R.id.buttonSubtract -> {
+                operation.text = "-"
+                res = num1 - num2
+            }
+            R.id.buttonMultiply -> {
+                operation.text = "*"
+                res = num1 * num2
+            }
+            R.id.buttonDivide -> {
+                operation.text = "/"
+                res = num1 / num2
+            }
+            R.id.buttonClean -> {
+                number1.setText("")
+                operation.text = ""
+                number2.setText("")
+                result.text = ""
+            }
+        }
+        result.text = res.toString()
     }
 }
